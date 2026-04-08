07dfee03a2fd74608c7eb9d5117b9fcf863f732a
Jennifer Lopez <hk7409@wayne.edu>
2026-04-08 13:17:13 -0400

    Reorganized project structure

--- print_lines.py
+++
@@ -1,7 +0,0 @@
-import sys
-path = r"c:\Users\varun\FindMySpot\database_setup.sql"
-with open(path) as f:
-    lines = f.readlines()
-for idx,line in enumerate(lines, start=1):
-    if 130 <= idx <= 170:
-        sys.stdout.write(f"{idx:3d} {line}")
